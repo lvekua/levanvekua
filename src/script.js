f0d1643420b3036e6bfa17109509b86f6fec8ebc
Levan Vekua <lvekua@gmail.com>
2022-03-18 12:15:58 -0400

    Refactored script.js

--- src/script.js
+++ src/script.js
@@ -41,21 +41,17 @@
     })();
 })(window, document);
 
-
 function navigationToggle(){
   const nav = document.querySelector("nav");
   const menuItem = document.querySelectorAll(".menu-item");
 
   menuItem.forEach(function(item){
     item.addEventListener("click", function(){
-      
       const menuToggle = document.querySelector(".menu-toggle");
-
       if(nav.classList.contains("is-open")){
         nav.classList.toggle("is-open");
         menuToggle.toggleAttribute("aria-expanded");  
       }
-      
     })
   });
 };
@@ -63,24 +59,22 @@ navigationToggle();
 
 function loaderSpinner(){
   window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader");
-
-  loader.classList.add("loader--hidden");
-
-  loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader);
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader--hidden");
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
   });
-});
 }
-loaderSpinner()
+loaderSpinner();
 
-function scrollIndicatorFunction() {
+function scrollIndicator() {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height =
-  document.documentElement.scrollHeight -
-  document.documentElement.clientHeight;
+  let scrollHeight = document.documentElement.scrollHeight;
+  let clientHeight = document.documentElement.clientHeight
+  let height = scrollHeight - clientHeight;
   let scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  document.querySelector(".progress-bar").style.width = scrolled + "%";
 };
 
 function navigationScroll() {
@@ -92,66 +86,7 @@ function navigationScroll() {
     }
   };
   
-  window.onscroll = function () {
-    scrollIndicatorFunction();
+  window.onscroll = function() {
+    scrollIndicator();
     navigationScroll();
   };
-
-  // Gsap scroll-triger
-  // function animateFrom(elem, direction) {
-  //   direction = direction || 1;
-  //   let x = 0,
-  //     y = direction * 100;
-  //   if (elem.classList.contains("gs_reveal_fromLeft")) {
-  //     x = -100;
-  //     y = 0;
-  //   } else if (elem.classList.contains("gs_reveal_fromRight")) {
-  //     x = 100;
-  //     y = 0;
-  //   } else if (elem.classList.contains("gs_reveal_fromBottom")) {
-  //     x = 0;
-  //     y = 100;
-  //   } else if (elem.classList.contains("gs_reveal_fromTop")) {
-  //     x = 0;
-  //     y = -100;
-  //   }
-  //   elem.style.transform = "translate(" + x + "px, " + y + "px)";
-  //   elem.style.opacity = "0";
-  //   gsap.fromTo(
-  //     elem,
-  //     { x: x, y: y, autoAlpha: 0 },
-  //     {
-  //       duration: 2.25,
-  //       x: 0,
-  //       y: 0,
-  //       autoAlpha: 1,
-  //       ease: "expo",
-  //       overwrite: "auto",
-  //     }
-  //   );
-  // }
-
-  // function hide(elem) {
-  //   gsap.set(elem, { autoAlpha: 0 });
-  // }
-
-  // document.addEventListener("DOMContentLoaded", function () {
-  //   gsap.registerPlugin(ScrollTrigger);
-
-  //   gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
-  //     hide(elem); // assure that the element is hidden when scrolled into view
-
-  //     ScrollTrigger.create({
-  //       trigger: elem,
-  //       onEnter: function () {
-  //         animateFrom(elem);
-  //       },
-  //       onEnterBack: function () {
-  //         animateFrom(elem, -1);
-  //       },
-  //       onLeave: function () {
-  //         hide(elem);
-  //       }, // assure that the element is hidden when scrolled into view
-  //     });
-  //   });
-  // });
